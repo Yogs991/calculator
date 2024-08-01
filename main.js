d327334654327f45985a1f6acd77d4c016e6fb53
Yog99 <124916155+Yogs991@users.noreply.github.com>
2024-08-01 20:02:38 +0300

    created the files

---
+++ main.js
@@ -0,0 +1,23 @@
+// const firstNumber
+// const secondNumber
+// const operator
+
+function add(){
+
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide(){
+
+}
+
+function operate(){
+
+}
