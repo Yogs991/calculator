10554148364f07b57d81985650c6a28bc772f320
Yog99 <124916155+Yogs991@users.noreply.github.com>
2024-08-03 12:36:21 +0300

    Added numbers to appear on display and clear button function

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-// const firstNumber
-// const secondNumber
-// const operator
+const firstNumber=0;
+const secondNumber = 0;
+const operator = "";
 
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clearBtn");
 const allBtns = document.querySelectorAll(".numberBtn");
+
 allBtns.forEach(element=>{
     element.addEventListener("click",buttons);
 });
 
-function buttons(e) {
-  const value = e.target.value;
-  document.getElementById("demo").innerHTML = value;  
+clear.addEventListener("click", clearDisplay);
+
+function buttons(input) {
+  const value = input.target.value;
+  display.value += value;
+}
+
+function clearDisplay(){
+  display.value = "";
 }
 
 function add(){
-
+  return firstNumber + secondNumber;
 }
 
 function subtract() {
-    
+    return firstNumber - secondNumber;
 }
 
 function multiply() {
-    
+    return firstNumber*secondNumber;
 }
 
 function divide(){
-
+  return firstNumber/secondNumber;
 }
 
 function operate(){
