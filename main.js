718b8b04e8d8be1fc8280ad50a8f334467e04d5e
Yog99 <124916155+Yogs991@users.noreply.github.com>
2024-08-11 20:10:34 +0300

    Added functionality to equl button

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-let number1="";
-let number2 = "";
-let operator = "";
-const display = document.querySelector("#display");
-const clear = document.querySelector("#clearBtn");
-const allBtns = document.querySelectorAll(".number");
-const operatorBtn = document.querySelectorAll(".operator");
-const equalButton = document.querySelector("#equalBtn");
-
-allBtns.forEach(element=>{
-    element.addEventListener("click",buttons);
-});
-
-operatorBtn.forEach(element=>{
-  element.addEventListener("click", buttons);// add new function to save second number when clicked
-});
-
-clear.addEventListener("click", clearDisplay);
-
-equalButton.addEventListener("click",operate);
-
-function buttons(input) {
-  if(operator ===""){
-    number1 = input.target.value;
-    display.value += number1;
-  }else{
-    number2 = input.target.value;
-  }
-}
-
-function clearDisplay(){
-  display.value = "";
-}
 function add(number1, number2){
   return number1 + number2;
 }
@@ -41,9 +8,12 @@ function multiply(number1, number2) {
     return number1*number2;
 }
 function divide(number1, number2){
-  return number1/number2;
+  if(number2 === 0){
+    return alert("undefined");
+  }else{
+    return number1/number2;
+  }
 }
-
 function operate(operator, number1, number2){
     switch(operator){
       case "+":
@@ -59,6 +29,56 @@ function operate(operator, number1, number2){
         return divide(number1, number2);
         break;
       default:
+        return "Invalid operator";
         break;
     }
-  }
+}
+
+let number1="";
+let number2 = "";
+let operator = "";
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clearBtn");
+const allBtns = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equalBtn");
+
+allBtns.forEach((element)=>{
+  element.addEventListener("click",(input)=>{
+    if(operator ===""){
+      number1 += input.target.value;
+      display.value += input.target.value;
+    }else{
+      number2 += input.target.value;
+      display.value += input.target.value;
+    }
+  });
+});
+
+operatorBtn.forEach((element)=>{
+  element.addEventListener("click", (input)=>{
+    operator = input.target.value;
+    display.value += input.target.value;
+  });
+});
+
+clear.addEventListener("click", ()=>{
+  display.value = "";
+  number1 = "";
+  number2 = "";
+  operator = "";
+});
+
+equalButton.addEventListener("click",equalPressed);
+
+function equalPressed(){
+    if(number1 !== "" && number2 !== "" && operator !==""){
+      const result = operate(operator, parseInt(number1), parseInt(number2));
+      display.value = result;
+      number1 = "";
+      number2 = "";
+      operator = "";
+    }
+}
+
+
